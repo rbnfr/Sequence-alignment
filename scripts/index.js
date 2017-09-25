18556ad5392bbc0b4a77d4acf9be3a65fae15984
Ruben_fr <ruben.f.alcaraz@gmail.com>
2017-09-25 16:49:34 +0200

    Angular tests

---
+++ scripts/index.js
@@ -0,0 +1,6 @@
+$('.exe-button').click(function() {
+  $('.progress-bar').css('display', '-webkit-box');
+  $('.progress-bar').removeAttr('ng-hide');
+  $('.progress-bar').Attr('ng-show', 'true');
+
+});
--- scripts/toolbar.js
+++ scripts/toolbar.js
@@ -4,51 +4,59 @@ angular.module('Alignment', ['ngMaterial'])
 
 })
 
-  .config(function($mdThemingProvider) {
-  })
-  .controller('AppCtrl', ['$scope', '$interval', function($scope, $interval) {
-    var self = this, j= 0, counter = 0;
+.config(function($mdThemingProvider) {
+})
 
-    self.mode = 'query';
-    self.activated = true;
-    self.determinateValue = 30;
-    self.determinateValue2 = 30;
+.controller('exe', function($scope){
 
-    self.showList = [ ];
 
-    /**
-     * Turn off or on the 5 themed loaders
-     */
-    self.toggleActivation = function() {
-        if ( !self.activated ) self.showList = [ ];
-        if (  self.activated ) {
-          j = counter = 0;
-          self.determinateValue = 30;
-          self.determinateValue2 = 30;
-        }
-    };
 
-    $interval(function() {
-      self.determinateValue += 1;
-      self.determinateValue2 += 1.5;
+})
 
-      if (self.determinateValue > 100) self.determinateValue = 30;
-      if (self.determinateValue2 > 100) self.determinateValue2 = 30;
 
-        // Incrementally start animation the five (5) Indeterminate,
-        // themed progress circular bars
+.controller('AppCtrl', ['$scope', '$interval', function($scope, $interval) {
+  var self = this, j= 0, counter = 0;
 
-        if ( (j < 2) && !self.showList[j] && self.activated ) {
-          self.showList[j] = true;
-        }
-        if ( counter++ % 4 === 0 ) j++;
+  self.mode = 'query';
+  self.activated = true;
+  self.determinateValue = 30;
+  self.determinateValue2 = 30;
 
-        // Show the indicator in the "Used within Containers" after 200ms delay
-        if ( j == 2 ) self.contained = "indeterminate";
+  self.showList = [ ];
 
-    }, 100, 0, true);
+  /**
+   * Turn off or on the 5 themed loaders
+   */
+  self.toggleActivation = function() {
+      if ( !self.activated ) self.showList = [ ];
+      if (  self.activated ) {
+        j = counter = 0;
+        self.determinateValue = 30;
+        self.determinateValue2 = 30;
+      }
+  };
 
-    $interval(function() {
-      self.mode = (self.mode == 'query' ? 'determinate' : 'query');
-    }, 7200, 0, true);
-  }]);
+  $interval(function() {
+    self.determinateValue += 1;
+    self.determinateValue2 += 1.5;
+
+    if (self.determinateValue > 100) self.determinateValue = 30;
+    if (self.determinateValue2 > 100) self.determinateValue2 = 30;
+
+      // Incrementally start animation the five (5) Indeterminate,
+      // themed progress circular bars
+
+      if ( (j < 2) && !self.showList[j] && self.activated ) {
+        self.showList[j] = true;
+      }
+      if ( counter++ % 4 === 0 ) j++;
+
+      // Show the indicator in the "Used within Containers" after 200ms delay
+      if ( j == 2 ) self.contained = "indeterminate";
+
+  }, 100, 0, true);
+
+  $interval(function() {
+    self.mode = (self.mode == 'query' ? 'determinate' : 'query');
+  }, 7200, 0, true);
+}]);
